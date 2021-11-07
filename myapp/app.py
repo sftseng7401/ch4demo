b74a1e6626b38199e31717da86dc7dc540e19ed7
ManticoreDai <a136724262@gmail.com>
2021-11-07 21:43:31 +0800

    first commit

--- myapp/app.py
+++ myapp/app.py
@@ -2,16 +2,20 @@ from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__, template_folder='templates/')
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/ping')
 def get_pong():
-    return jsonify({'message':'pong'})
+    return jsonify({'message': 'pong'})
+
 
 @app.route('/pong')
 def get_ping():
-    return jsonify({'message':'ping'})
+    return jsonify({'message': 'ping'})
 
-app.run(debug=True, host='0.0.0.0', port=5000)
+
+app.run(debug=True, host='0.0.0.0', port=8000)
