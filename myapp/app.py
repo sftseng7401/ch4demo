2205025ce1c7038cb35eceabf28bf95f04f58f44
ManticoreDai <a136724262@gmail.com>
2021-11-08 20:54:06 +0800

    test cool!

--- myapp/app.py
+++ myapp/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder='templates/')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', text="Cool！")
 
 
 @app.route('/ping')
@@ -20,5 +20,6 @@ def get_ping():
 
 
 # test
+# 一定要加這個，不然部署到heroku上時，他不會去抓 heroku提供的環境變數中的 port
 port = int(os.environ.get('PORT', 5000))
 app.run(debug=True, host='0.0.0.0', port=port)
