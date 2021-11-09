309c3134208c8b75d3ab8677e14a1295dc7b26a9
ManticoreDai <a136724262@gmail.com>
2021-11-09 19:45:35 +0800

    add ppt and download

--- myapp/app.py
+++ myapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, send_from_directory
 import os
 
 app = Flask(__name__, template_folder='templates/')
@@ -6,7 +6,13 @@ app = Flask(__name__, template_folder='templates/')
 
 @app.route('/')
 def home():
-    return render_template('index.html', text="Cool！")
+    return render_template('index.html',
+                           text="Cool！")
+
+
+@app.route('/download')
+def download_ppt():
+    return send_from_directory("./", "Chapter4.pptx")
 
 
 @app.route('/ping')
@@ -23,3 +29,5 @@ def get_ping():
 # 一定要加這個，不然部署到heroku上時，他不會去抓 heroku提供的環境變數中的 port
 port = int(os.environ.get('PORT', 5000))
 app.run(debug=True, host='0.0.0.0', port=port)
+
+# app.run(host='127.0.0.1', port=5000)
