9bf5ab61e19034ea18c47ee0a977ce30e77d76c5
ManticoreDai <a136724262@gmail.com>
2021-11-09 22:31:01 +0800

    update ppt

--- myapp/app.py
+++ myapp/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__, template_folder='templates/')
 @app.route('/')
 def home():
     return render_template('index.html',
-                           text="Cool！")
+                           text="Cool！")  # change this
 
 
 @app.route('/download')
